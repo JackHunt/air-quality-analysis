64b6b9cf8a543e122d3721c7c7746085af1394ee
Jack Hunt <jackmileshunt@gmail.com>
2025-12-20 12:12:33 +0000

    Update purple_air.R

--- src/purple_air.R
+++ src/purple_air.R
@@ -22,14 +22,46 @@ do_get <- function(id, fields, time_avg, api_key) {
   read.csv(text = res)
 }
 
+do_get_metadata <- function(id, api_key) {
+  fields_str <- paste(
+    c(
+      "location_type", # 0 is outside
+      "latitude",
+      "longitude",
+      "altitude"
+    ),
+    collapse = ","
+  )
+
+  url <- paste0(
+    "https://api.purpleair.com/v1/sensors/",
+    id
+  )
+
+  request(url) |>
+    req_url_query(fields = fields_str) |>
+    req_headers(`X-API-Key` = api_key) |>
+    req_perform(verbosity = 3) |>
+    resp_body_json()
+}
+
 retrieve_data <- function(config_path, output_path, api_key) {
   config <- read_yaml(config_path)
 
   res <- lapply(config$sensors, function(sensor) {
-    do_get(sensor$id, config$fields, config$time_average, api_key)
+    metadata <- do_get_metadata(sensor$id, api_key)
+
+    do_get(sensor$id, config$fields, config$time_average, api_key) %>%
+      mutate(
+        sensor_name = metadata$sensor$name,
+        is_indoor = metadata$sensor$location_type,
+        latitude = metadata$sensor$latitude,
+        longitude = metadata$sensor$longitude
+      )
   })
 
-  res_combined <- bind_rows(res)
+  res_combined <- bind_rows(res) %>%
+    arrange(sensor_index, time_stamp)
   saveRDS(res_combined, output_path)
 }
 

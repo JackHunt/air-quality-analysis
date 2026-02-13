a8eb96a8d0216d5937281978f0c32b7876d79e27
Jack Hunt <jackmileshunt@gmail.com>
2026-02-13 12:03:41 +0000

    Update single_sensor_vis.R

--- src/single_sensor_vis.R
+++ src/single_sensor_vis.R
@@ -4,29 +4,26 @@ library(bslib)
 library(ggplot2)
 library(patchwork)
 
-plot_ts <- function(df, x, y, title) {
-  ggplot(df, aes_string(x = x, y = y)) +
-    geom_line() +
-    ggtitle(title)
-}
-
 plot_raw <- function(df) {
+  plot_ts <- function(x, y, title) {
+    ggplot(df, aes_string(x = x, y = y)) +
+      geom_line() +
+      ggtitle(title)
+  }
+
   p1 <- plot_ts(
-    df,
     "time_stamp",
     "temperature",
     "Temperature"
   )
 
   p2 <- plot_ts(
-    df,
     "time_stamp",
     "pressure",
     "Pressure"
   )
 
   p3 <- plot_ts(
-    df,
     "time_stamp",
     "humidity",
     "Humidity"
@@ -35,9 +32,24 @@ plot_raw <- function(df) {
   p1 + p2 + p3
 }
 
+plot_posterior_mean <- function(df) {
+  ggplot(df, aes(x = time_stamp)) +
+    geom_line(aes(y = mu_f, color = "Posterior Mean")) +
+    geom_ribbon(
+      aes(
+        ymin = mu_f - sqrt(var_f),
+        ymax = mu_f + sqrt(var_f)
+      ),
+      alpha = 0.1
+    ) +
+    geom_line(aes(y = pm2.5_alt, color = "True PM2.5")) +
+    ggtitle("PM2.5 Prediction")
+}
+
 run_shiny <- function(args) {
   output <- readRDS(file.path(args$input_path, "output.rds"))
-  df <- output$pred
+  df_pred <- output$pred
+  df_fit <- output$fit
 
   ui <- pageWithSidebar(
     headerPanel("Air Quality"),
@@ -45,12 +57,16 @@ run_shiny <- function(args) {
       dateRangeInput("dates", "Select dates")
     ),
     mainPanel(
-      plotOutput("main_plot")
+      plotOutput("raw_plot"),
+      plotOutput("pred_plot"),
+      plotOutput("fit_plot"),
     )
   )
 
   server <- function(input, output) {
-    output$main_plot <- renderPlot(plot_raw(df))
+    output$raw_plot <- renderPlot(plot_raw(df_pred))
+    output$pred_plot <- renderPlot(plot_posterior_mean(df_pred))
+    output$fit_plot <- renderPlot(plot_posterior_mean(df_fit))
   }
 
   shinyApp(ui = ui, server = server)
@@ -69,5 +85,6 @@ if (!interactive()) {
 
   args <- parse_args(opt_parser)
 
+  options(shiny.port = 8100)
   run_shiny(args)
 }
